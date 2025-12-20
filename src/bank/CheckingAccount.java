2faf3448ab68c52d1726f3160c8530279c842585
melisagrlk <melisagrlk102938@gmail.com>
2025-12-20 09:30:57 +0300

    feat: implement SavingsAccount with interest rate logic

--- src/bank/CheckingAccount.java
+++ src/bank/CheckingAccount.java
@@ -1,5 +1,21 @@
 package bank;
 
-public class CheckingAccount {
+public class CheckingAccount extends Account {
+    private double overdraftLimit;
 
+    public CheckingAccount(String accountNumber, double balance, double overdraftLimit) {
+        super(accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        if (amount > 0 && (getBalance() + overdraftLimit) >= amount) {
+            setBalance(getBalance() - amount);
+            transactions.add(new Transaction("Withdrawal (Overdraft)", amount));
+            return true;
+        }
+        System.out.println("Overdraft limit exceeded!");
+        return false;
+    }
 }
--- src/bank/SavingsAccount.java
+++ src/bank/SavingsAccount.java
@@ -1,5 +1,26 @@
 package bank;
 
-public class SavingsAccount {
-
+public class SavingsAccount extends Account {
+	private double interestRate;
+	
+	public SavingsAccount(String accountNumber, double balance, double interestRate) {
+		super(accountNumber, balance);
+		this.interestRate=interestRate;
+	}
+	
+	public boolean withdraw(double amount) {
+		if(amount>0 && getBalance()>=amount) {
+			setBalance(getBalance() - amount);
+            transactions.add(new Transaction("Withdrawal", amount));
+            return true;
+        }
+        System.out.println("Insufficient funds in Savings Account!");
+        return false;
+    }
+	
+	public void applyInterest() {
+        double interest = getBalance() * (interestRate / 100);
+        deposit(interest);
+        System.out.println("Interest applied: " + interest);
+    }
 }
