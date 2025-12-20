8d5354ceeb940dd2b27f3a3a362fc5d826014118
melisagrlk <melisagrlk102938@gmail.com>
2025-12-20 09:52:22 +0300

    feat: implement CheckingAccount with overdraft limit logic

--- src/bank/Account.java
+++ src/bank/Account.java
@@ -1,5 +1,64 @@
 package bank;
 
-public class Account {
+import java.util.ArrayList;
 
+public abstract class Account implements Transferable {
+
+    protected String accountNumber;
+    private double balance;
+    protected ArrayList<Transaction> transactions = new ArrayList<>();
+
+    public Account(String accountNumber, double balance) {
+        if (balance < 0) {
+            throw new IllegalArgumentException(
+                "Initial balance cannot be negative"
+            );
+        }
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException(
+                "Deposit amount must be positive"
+            );
+        }
+        balance += amount;
+        transactions.add(new Transaction("Deposit", amount));
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    protected void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public abstract boolean withdraw(double amount);
+
+    @Override
+    public boolean transfer(Account toAccount, double amount) {
+
+        if (toAccount == null) {
+            throw new IllegalArgumentException(
+                "Target account cannot be null"
+            );
+        }
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException(
+                "Transfer amount must be positive"
+            );
+        }
+
+        if (this.withdraw(amount)) {
+            toAccount.deposit(amount);
+            transactions.add(new Transaction("Transfer", amount));
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/bank/Transferable.java
+++ src/bank/Transferable.java
@@ -1,5 +1,13 @@
 package bank;
 
 public interface Transferable {
+	
+	/**
+     * Performs a balance transfer from this account to a target account.
+     * @param toAccount The destination account for the transfer.
+     * @param amount The amount of money to be transferred.
+     */
+	
+	boolean transfer(Account toAccount, double amount);
 
 }
