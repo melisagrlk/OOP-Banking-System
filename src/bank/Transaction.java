51bcd9b984037c6f424b2737a83f316f16ea4be7
melisagrlk <melisagrlk102938@gmail.com>
2025-12-25 01:09:46 +0300

    feat: add timestamp to Transaction and format output

--- src/bank/Transaction.java
+++ src/bank/Transaction.java
@@ -1,12 +1,15 @@
 package bank;
+import java.time.LocalDateTime;
 
 public class Transaction {
     private String type; // ex: "Deposit", "Withdrawal"
     private double amount;
+    private LocalDateTime timestamp;
 
     public Transaction(String type, double amount) {
         this.type = type;
         this.amount = amount;
+        this.timestamp = LocalDateTime.now(); // Record the time of the transaction.
     }
 
     public String getType() {
@@ -15,5 +18,9 @@ public class Transaction {
 
     public double getAmount() {
         return amount;
+    }
+    
+    public LocalDateTime getTimestamp() {
+    	return timestamp;
     }
 }
